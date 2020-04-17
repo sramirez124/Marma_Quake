b0b5b7cc1a7c67d88df685f2f64ac7cdd1c3bc82
Steven-Ramirez <sramriez124@gmail.com>
2020-04-16 22:07:20 -0500

    Final Test before Release
    Placed down rockeet spawn and fixed platforms on lava

--- working/compile.bat
+++ working/compile.bat
@@ -5,26 +5,25 @@ cd C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
 
 
 echo Copying Files...
-copy C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps\magma.map C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
+copy C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps\lava.map C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\qbsp.exe magma
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\qbsp.exe lava
 
 echo --------------VIS---------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\vis.exe magma
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\vis.exe lava
 
 echo -------------LIGHT--------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\light.exe -soft magma
-pause
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\light.exe -soft lava
 
-copy magma.bsp C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
-copy magma.pts C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
-copy magma.lit C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy lava.bsp C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy lava.pts C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy lava.lit C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
 pause
 cd\
 cd C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel
-quakespasm  +map magma
+quakespasm  +map lava
