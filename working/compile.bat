c874ce3b1e05f631b4483d4454c031043d5052b5
Steven-Ramirez <sramriez124@gmail.com>
2020-04-16 21:17:10 -0500

    Moved items around
    Balanced item placement

--- working/compile.bat
+++ working/compile.bat
@@ -5,26 +5,26 @@ cd C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
 
 
 echo Copying Files...
-copy C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps\marma.map C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
+copy C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps\magma.map C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\qbsp.exe marma
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\qbsp.exe magma
 
 echo --------------VIS---------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\vis.exe marma
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\vis.exe magma
 
 echo -------------LIGHT--------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\light.exe -soft marma
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\light.exe -soft magma
 pause
 
-copy marma.bsp C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
-copy marma.pts C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
-copy marma.lit C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy magma.bsp C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy magma.pts C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy magma.lit C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
 pause
 cd\
 cd C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel
-quakespasm  +map marma
+quakespasm  +map magma
