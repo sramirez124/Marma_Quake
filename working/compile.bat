999fdb5259cb1a3ab482097339ea5d401f7ea2ad
Steven-Ramirez <sramriez124@gmail.com>
2020-04-23 14:38:03 -0500

    Added Quakespasm and Trenchbroom to Github

--- working/compile.bat
+++ working/compile.bat
@@ -5,25 +5,25 @@ cd C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
 
 
 echo Copying Files...
-copy C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps\lava.map C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
+copy C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps\marma.map C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\working
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\qbsp.exe lava
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\qbsp.exe marma
 
 echo --------------VIS---------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\vis.exe lava
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\vis.exe marma
 
 echo -------------LIGHT--------------
-C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\light.exe -soft lava
+C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\tools\ericw-tools\bin\light.exe -soft marma
 
-copy lava.bsp C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
-copy lava.pts C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
-copy lava.lit C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy marma.bsp C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy marma.pts C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
+copy marma.lit C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel\id1\maps
 pause
 cd\
 cd C:\Users\Steve\Documents\GitHub\quakemultiplayerlevel
-quakespasm  +map lava
+quakespasm  +map marma
